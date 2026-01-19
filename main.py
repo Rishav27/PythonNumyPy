73879a1fb5fdf27d3e63e4555a865b3be1b34bca
RishavRoshan <rishabhroshan27@gmail.com>
2026-01-19 15:49:46 +0530

    v0.1.1(MultiDmensionalArray)

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import numpy
-
-
-def main():
-    print("Hello from pythonnumpy!")
-
+import numpy as np
 
 if __name__ == "__main__":
-    main()
+    # 1D array
+    arr = np.array([1, 2, 3])
+
+    # 2D array
+    arr2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    # 3D array
+    array = np.array(
+        [
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
+            [[19, 20, 21], [22, 23, 24], [25, 26, 27]],
+        ]
+    )
+
+    print(array)
+    print(array.ndim)
