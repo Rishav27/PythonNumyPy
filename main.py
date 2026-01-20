ca3c2c088a29012e7a9de956ef9595c097728596
RishavRoshan <rishabhroshan27@gmail.com>
2026-01-20 15:54:40 +0530

    v0.1.3(arithmetic  opertion)

--- main.py
+++ main.py
@@ -22,3 +22,32 @@ if __name__ == "__main__":
     # print(arr[0:2, 2:4])
     # print(arr[2:4, 0:2])
     # print(arr[2:4, 2:4])
+
+    arrary = np.array([1, 2, 3])
+
+    # Scaler arathmetic operations
+
+    # print(arrary + 1)
+    # print(arrary - 2)
+    # print(arrary * 3)
+    # print(arrary / 4)
+    # print(arrary ** 5)
+
+    # Vector maths func
+    arrary = np.array([1.22, 2.02, 3.99])
+
+    print(np.sqrt(arrary))
+    print(np.round(arrary))  # can alse use floor and ceil
+    print(np.pi)
+
+    # ecercise
+
+    radii = np.array([1, 2, 3])
+    print(np.pi * radii**2)
+
+    # comparison operations
+
+    marks = np.array([55, 89, 76, 45, 91])
+    print(marks < 60)
+    marks[marks < 60] = 0
+    print(marks)
